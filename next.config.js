11f43f30feef29c25f8fbc2713b5a3f9ef80b443
vocke4 <129233076+vocke4@users.noreply.github.com>
2025-01-26 21:50:06 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 // next.config.js
 module.exports = {
   reactStrictMode: true,
+  transpilePackages: [
+    'react-markdown',
+    'remark-gfm',
+    'unist-util-position',
+    'mdast-util-from-markdown',
+    'micromark',
+    'vfile'
+  ],
+  experimental: {
+    esmExternals: 'loose',
+  },
 };
